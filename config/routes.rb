65658fe755b5b305f201e3bebf990a425c0223b9
Simon Tharby <simon@liquidvoting.io>
2021-06-17 14:00:02 +0100

    Add comments to routes
    Nest within projects resources
    Limit to create only

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :projects
+  resources :projects do
+    resources :comments, only: [:create]
+  end
+
   root to: 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
